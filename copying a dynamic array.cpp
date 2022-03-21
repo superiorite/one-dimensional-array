54217b5fe0b138d68cc1b225b3a65341c0bb2fa2
superiorite <56066154+superiorite@users.noreply.github.com>
2022-03-21 19:44:13 +0300

    Create copying a dynamic array.cpp

---
+++ copying a dynamic array.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+void FillArray(int* const arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+
+void ShowArray(const int* const arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int size = 10;
+	int* firstArray = new int[size];
+	int* secondArray = new int[size];
+
+	FillArray(firstArray, size);
+	FillArray(secondArray, size);
+
+	cout << "FirstArray =\t";
+	ShowArray(firstArray, size);
+	cout << "SecondArray =\t";
+	ShowArray(secondArray, size);
+
+	delete[] firstArray;
+
+	firstArray = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		firstArray[i] = secondArray[i];
+	}
+
+	cout << "=================================" << endl;
+
+	cout << "FirstArray \t";
+	ShowArray(firstArray, size);
+	cout << "SecondArray \t";
+	ShowArray(secondArray, size);
+
+	delete[] firstArray;
+	delete[] secondArray;
+}
